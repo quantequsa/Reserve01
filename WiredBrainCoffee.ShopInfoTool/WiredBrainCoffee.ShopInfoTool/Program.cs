264c1267e0213a932e985262ca3143786b44113f
Quanteq <Quanteq@hotmail.com>
2020-10-29 08:31:49 -0500

    11 BASE SECOND Commit WiredBrainCoffee.ShopInfoTool project files

---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            Console.ReadLine();
+        }
+    }
+}
